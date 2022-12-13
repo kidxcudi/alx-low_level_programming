58edb586bf28ae7a96783fe44f84d3dd0ac0df42
kidxcudi <112644403+kidxcudi@users.noreply.github.com>
2022-12-13 18:48:18 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -38,10 +38,10 @@ int main(void)
 
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
-			print(', ');
+			printf(', ');
 
 		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_hal;
+		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
 		fib2_half2 = half2;
 	}
