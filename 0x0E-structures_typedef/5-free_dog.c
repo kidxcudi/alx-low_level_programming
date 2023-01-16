10f6c519b606e8e5b5f57833c427fb0e8a6e572b
kidxcudi <112644403+kidxcudi@users.noreply.github.com>
2023-01-17 01:33:50 +0300

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,16 @@
-dasd
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees the memory of dog struct.
+ * @d: dog struct
+ * Return: pointer to the new dog.
+ */
+void free_dog(dog_t *d)
+{
+	if (!d)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
