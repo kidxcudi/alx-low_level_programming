789b5b3c1ec03e9d0dcaf7fc60ac4634a9c31603
kidxcudi <112644403+kidxcudi@users.noreply.github.com>
2023-01-26 14:09:18 +0300

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,1 +1,13 @@
-djasldk
+section .data
+
+    message db "Hello, Holberton", 10
+
+section .text
+
+global main
+main:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, message
+    mov rdx, 17
+    syscall
