6ba73cf58955121e86c670d6102422d7459230fb
kidxcudi <112644403+kidxcudi@users.noreply.github.com>
2023-01-31 00:09:55 +0300

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,36 +1,28 @@
 #include "lists.h"
+
 /**
- * find_listint_loop - found a loop and the begining of loop
- * @head: pointer to head in function
- * Return: pointer to the begining of loop
+ * print_listint_safe - prints a linked list, safely
+ * @head: list of type listint_t to print
+ * Return: number of nodes in the list
  */
-listint_t *find_listint_loop(listint_t *head)
+size_t print_listint_safe(const listint_t *head)
 {
-	listint_t *tortoise;
-	listint_t *hare;
+	size_t num = 0;
+	long int diff;
 
-	if (head == NULL || head->next == NULL)/*If the list is null or just 1 node*/
-		return (NULL);
-
-	tortoise = hare = head;/*Start at the same point*/
-
-	while (tortoise && hare && hare->next)
+	while (head)
 	{
-		tortoise = tortoise->next;/*1 step hop*/
-		hare = hare->next->next;/*2 steps hop*/
-		if (tortoise == hare)
+		diff = head - head->next;
+		num++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;
+		}
 	}
 
-	if (tortoise != hare) /* if the while ended until a NULL*/
-		return (NULL);
-
-	tortoise = head; /*re-start tortoise*/
-
-	while (tortoise != hare)
-	{
-		tortoise = tortoise->next;
-		hare = hare->next;
-	}
-	return (hare);
+	return (num);
 }
