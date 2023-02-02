06ae625045e6a3cd4e048e081b899fa0737077c4
kidxcudi <112644403+kidxcudi@users.noreply.github.com>
2023-02-02 19:06:43 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,27 +1,26 @@
 #include "main.h"
+
 /**
- * print_binary - translate number to binary.
- * @n: the input number
- * Return: binary number started by one
+ * print_binary - prints the binary of a decimal number
+ * @n: number to print in binary
  */
 void print_binary(unsigned long int n)
 {
-	int i, j = 0;
-	unsigned long int temp, mask = 0;
+	int i, count = 0;
+	unsigned long int current;
 
-	if (n == 0)
-		_putchar('0');
-	temp = n;
-
-	for (j = 0; temp != 0; j++)/*Ignoring left-zeros*/
-		temp = temp >> 1;
-
-	for (i = j - 1; i >= 0; i--)
+	for (i = 63; i >= 0; i--)
 	{
-		mask = 1 << i;/*Creating a mask for extract the digit*/
-		if ((n & mask) != 0)
+		current = n >> i;
+
+		if (current & 1)
+		{
 			_putchar('1');
-		else
+			count++;
+		}
+		else if (count)
 			_putchar('0');
 	}
+	if (!count)
+		_putchar('0');
 }
