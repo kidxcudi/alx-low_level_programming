d85693e58b38de6022fd6772614f82dcd6bbdecf
Kidxcudi <kidxcudi@gmail.com>
2024-04-28 15:29:04 -0700

    commit

--- 0x1A-hash_tables/100-main.c
+++ 0x1A-hash_tables/100-main.c
@@ -10,26 +10,26 @@
  */
 int main(void)
 {
-    shash_table_t *ht;
+    hash_table_t *ht;
 
-    ht = shash_table_create(1024);
-    shash_table_set(ht, "y", "0");
-    shash_table_print(ht);
-    shash_table_set(ht, "j", "1");
-    shash_table_print(ht);
-    shash_table_set(ht, "c", "2");
-    shash_table_print(ht);
-    shash_table_set(ht, "b", "3");
-    shash_table_print(ht);
-    shash_table_set(ht, "z", "4");
-    shash_table_print(ht);
-    shash_table_set(ht, "n", "5");
-    shash_table_print(ht);
-    shash_table_set(ht, "a", "6");
-    shash_table_print(ht);
-    shash_table_set(ht, "m", "7");
-    shash_table_print(ht);
-    shash_table_print_rev(ht);
-        shash_table_delete(ht);
+    ht = hash_table_create(1024);
+    hash_table_set(ht, "y", "0");
+    hash_table_print(ht);
+    hash_table_set(ht, "j", "1");
+    hash_table_print(ht);
+    hash_table_set(ht, "c", "2");
+    hash_table_print(ht);
+    hash_table_set(ht, "b", "3");
+    hash_table_print(ht);
+    hash_table_set(ht, "z", "4");
+    hash_table_print(ht);
+    hash_table_set(ht, "n", "5");
+    hash_table_print(ht);
+    hash_table_set(ht, "a", "6");
+    hash_table_print(ht);
+    hash_table_set(ht, "m", "7");
+    hash_table_print(ht);
+    hash_table_print_rev(ht);
+        hash_table_delete(ht);
     return (EXIT_SUCCESS);
 }
